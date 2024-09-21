0317fd8cfba65f817d623068b5cd246cb6d4bf20
AJAY VENKATESH BABU <ajayvengateshbabu@gmail.com>
2024-09-21 08:58:08 +0300

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
 //your JS code here. If required.
+
+
+const circles = document.querySelectorAll(".circle");
+const lines = document.querySelectorAll(".line");
+
+const nextButton = document.querySelectorAll("#next");
+const prevButton = document.querySelectorAll("#prev");
+
+let val = 1;
+
+nextButton.addEventListener("click", ()=> {
+	val++;
+	updateUi();
+})
+
+prevButton.addEventListener("click", () =>{
+	val--;
+	updateUi();
+})
+
+function updateUi(){
+	circles.forEach((circle, idx) => {
+		idx<val?circle.classList.add("active"):circle.classList.remove("active");
+	})
+
+	lines.forEach((line,idx) => {
+		idx<val?line.classList.add("active"):line.classList.remove("active");
+	})
+
+	prevButton.disabled = val===1;
+	nextButton.disabled = val===circles.length;
+}
