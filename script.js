4a5761b4f5a5a94e9fed695df6d51bf27a20b825
AJAY VENKATESH BABU <ajayvengateshbabu@gmail.com>
2024-09-21 09:04:24 +0300

    created a submission

--- script.js
+++ script.js
@@ -4,30 +4,38 @@
 const circles = document.querySelectorAll(".circle");
 const lines = document.querySelectorAll(".line");
 
-const nextButton = document.querySelectorAll("#next");
-const prevButton = document.querySelectorAll("#prev");
+const nextButton = document.getElementById("next");
+const prevButton = document.getElementById("prev");
 
 let val = 1;
 
 nextButton.addEventListener("click", ()=> {
 	val++;
 	updateUi();
-})
+});
 
 prevButton.addEventListener("click", () =>{
 	val--;
 	updateUi();
-})
+});
 
 function updateUi(){
-	circles.forEach((circle, idx) => {
-		idx<val?circle.classList.add("active"):circle.classList.remove("active");
-	})
+	 circles.forEach((circle, idx) => {
+        if (idx < val) {
+            circle.classList.add("active");
+        } else {
+            circle.classList.remove("active");
+        }
+    });
 
-	lines.forEach((line,idx) => {
-		idx<val?line.classList.add("active"):line.classList.remove("active");
-	})
+    lines.forEach((line, idx) => {
+        if (idx < val - 1) {
+            line.classList.add("active");
+        } else {
+            line.classList.remove("active");
+        }
+    });
 
-	prevButton.disabled = val===1;
-	nextButton.disabled = val===circles.length;
+	prevButton.disabled = val === 1;
+	nextButton.disabled = val === circles.length;
 }
